271d43c84da25ccd814c5bbc09daa3e8fb02dcb5
Mikkel Faartoft Jensen <mikkelfaartoftjensen@gmail.com>
2020-03-19 02:19:49 +0100

    expierence section

--- web/pages/expierence.js
+++ web/pages/expierence.js
@@ -11,12 +11,119 @@ export default class ExpierencePage {
      
       <div id="expierence__developer" class="expierence__containers">
       <aside class="expierence__containers__text">
-       
-      </aside>
-      <div class="expierence__containers__list">
       
+      <img src="http://faartoft.com/images/icons/code.svg" alt="code icon">  
+      <h3>Developing</h3> 
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing </p>
+      </aside>
+      
+      <div class="expierence__containers__list">
+      <div class="expierence__containers__list--wrapper">
+      <h4>Languages</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/html-white.svg" alt="HTML icon white">HTML5</li>
+      <li><img src="http://faartoft.com/images/white-icons/css-white.svg" alt="css icon white">CSS3</li>
+      <li><img src="http://faartoft.com/images/white-icons/sass-white.svg" alt="sass icon white">Sass</li>
+      <li><img src="http://faartoft.com/images/white-icons/javascript-white.svg" alt="js icon white">JavaScript</li>
+      <li><img src="http://faartoft.com/images/white-icons/jquery-white.svg" alt="jquery icon white">JQuery</li>
+      <li><img src="http://faartoft.com/images/white-icons/json-white.svg" alt="json icon white">JSON</li>
+      <li><img src="http://faartoft.com/images/white-icons/php-white.svg" alt="php icon white">PHP</li>
+      </ul>
+      </div>
+      <div class="expierence__containers__list--wrapper">
+      <h4>Git</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/git-white.svg" alt="git icon white">Git</li>
+      <li><img src="http://faartoft.com/images/white-icons/github-white.svg" alt="github icon white">Github</li>
+      <li><img src="http://faartoft.com/images/white-icons/gitkraken-white.svg" alt="gitkraken icon white">Gitkraken</li>
+      <li><img src="http://faartoft.com/images/white-icons/bitbucket-white.svg" alt="bitbucket icon white">Bitbucket</li>
+      </ul>
+      </div>
+      <div class="expierence__containers__list--wrapper">
+      <h4>Database</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/firebase-white.svg" alt="firebase icon white">Firebase</li>
+      <li><img src="http://faartoft.com/images/white-icons/heroku-white.svg" alt="heroku icon white">Heroku</li>
+      <li><img src="http://faartoft.com/images/white-icons/wordpress-white.svg" alt="wordpress icon white">WordPress (headless CMS)</li> 
+      </ul>
       </div>
       </div>
+      </div>
+
+
+
+
+      <!-------->
+      <div id="expierence__method" class="expierence__containers">
+    
+      <div class="expierence__containers__list">
+      <div class="expierence__containers__list--wrapper">
+      <h4>Design</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/website-design.svg" alt="website design icon white">Web design</li>
+      <li><img src="http://faartoft.com/images/white-icons/pen-white.svg" alt="UI & UX">UI & UX</li>
+      <li><img src="http://faartoft.com/images/white-icons/laptop-white.svg" alt="responsive web icon white">Respnsive web design</li>
+      <li><img src="http://faartoft.com/images/white-icons/sketch-white.svg" alt="sketch icon white">Prototyping</li>
+      </ul>
+      </div>
+      <div class="expierence__containers__list--wrapper">
+      <h4>Model</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/refresh-white.svg" alt="Agile icon white">Agile</li>
+      <li><img src="http://faartoft.com/images/white-icons/refresh-white.svg" alt="Scrum icon white">SCRUM</li>
+      <li><img src="http://faartoft.com/images/white-icons/refresh-white.svg" alt="Sprint icon white">Sprint</li>
+      </ul>
+      </div>
+      <div class="expierence__containers__list--wrapper">
+      <h4>Approach</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/smartphone-white.svg" alt="mobile first icon white">Mobile first</li>
+      <li><img src="http://faartoft.com/images/white-icons/smartphone-white.svg" alt="Single page application icon white">SPA</li>
+      <li><img src="http://faartoft.com/images/white-icons/performance-white.svg" alt="performance icon white">Performance</li> 
+      </ul>
+      </div>
+      </div>
+      <aside class="expierence__containers__text">
+      
+      <img src="http://faartoft.com/images/icons/method.svg" alt="code icon">  
+      <h3>Method</h3> 
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing </p>
+      </aside>
+      </div>
+
+      <div id="expierence__developer" class="expierence__containers">
+      <aside class="expierence__containers__text">
+      
+      <img src="http://faartoft.com/images/icons/tools.svg" alt="tools icon">  
+      <h3>Tools</h3> 
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing </p>
+      </aside>
+      
+      <div class="expierence__containers__list">
+      <div class="expierence__containers__list--wrapper">
+      <h4>Developer tools</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/vscode-white.svg" alt="visual studio code icon white">Visual Studio Code</li>
+      <li><img src="http://faartoft.com/images/white-icons/atom-white.svg" alt="atom icon white">Atom</li>
+      <li><img src="http://faartoft.com/images/white-icons/emmet-white.svg" alt="emmet icon white">Emmet</li>
+      <li><img src="http://faartoft.com/images/white-icons/bootstrap-white.svg" alt="bootstrap icon white">Bootstrap</li>
+      <li><img src="http://faartoft.com/images/white-icons/koala-white.svg" alt="koala compiler icon white">Koala</li>
+      <li><img src="http://faartoft.com/images/white-icons/filezilla-white.svg" alt="filezilla icon white">Filezilla</li>
+      </ul> 
+      </div>
+      <div class="expierence__containers__list--wrapper">
+      <h4>Design tools</h4>
+      <ul>
+      <li><img src="http://faartoft.com/images/white-icons/adobephotoshop-white.svg" alt="photoshop icon white">Photoshop</li>
+      <li><img src="http://faartoft.com/images/white-icons/adobexd-white.svg" alt="adobe xd icon white">XD</li>
+      <li><img src="http://faartoft.com/images/white-icons/adobeindesign-white.svg" alt="indesign icon white">InDesign</li>
+      <li><img src="http://faartoft.com/images/white-icons/adobeillustrator-white.svg" alt="illustrator icon white">Illustrator</li>
+      </ul>
+      </div>
+      
+      </div> 
+      </div>
+
       `;
     } 
   } 
